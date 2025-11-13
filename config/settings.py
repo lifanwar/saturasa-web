a87e2396bbbf50bcafa0795244d9a4e68572aea6
lifanwar <mualifulanwar1@gmail.com>
2025-11-13 19:24:59 +0200

    menambahkan untuk csrf dan allowed hosted ke cloudflared tunnel

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -242,3 +242,7 @@ SOCIALACCOUNT_EMAIL_REQUIRED = True
 
 # END DJANGO ALLAUTH CONFIGURATION
 # ============================================
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.trycloudflare.com',  # Wildcard untuk semua subdomain
+]
