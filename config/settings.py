f0291cc4b66287297cd5759a00a4c2747d74ec68
lifanwar <mualifulanwar1@gmail.com>
2025-11-20 18:17:48 +0200

    menghapus deprecated full auth

--- config/settings.py
+++ config/settings.py
@@ -179,7 +179,6 @@ ACCOUNT_SIGNUP_FIELDS = [
 
 # User model configuration
 ACCOUNT_USER_MODEL_USERNAME_FIELD = 'username'# No username field
-ACCOUNT_USERNAME_REQUIRED = False 
 ACCOUNT_USER_MODEL_EMAIL_FIELD = 'email'  # Email field name
 
 # ===== Email Configuration =====
